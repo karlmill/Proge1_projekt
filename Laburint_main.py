4b357d293f650c9e4dce0893cf69621032c78c9e
Karl Miller <mkarl418@gmail.com>
2025-12-15 01:31:03 +0200

    Koodi struktuur, käivitamiseks käima main.py

---
+++ Laburint_main.py
@@ -0,0 +1,62 @@
+#https://www.pygame.org/docs/
+#Projekt Bive, Lilii-Ann Older, Karl Miller
+#Mängu mängimiseks (programmi käivitamiseks) on vajalik alla laadida pygame.
+#Allalaadimseks vaja terminali sisestada käsk "pip3 install pygame"
+import pygame
+import time
+import sys
+from Projekt_level1 import level_1
+from Projekt_level2 import level_2
+from Projekt_level3 import level_3
+from Projekt_level4 import level_4
+from Projekt_level5 import level_5
+
+
+pygame.init()
+screen=pygame.display.set_mode((640, 640))
+pygame.display.set_caption('Proge projekt')
+
+hiir = pygame.image.load('hiir.png').convert()
+hiir = pygame.transform.scale(hiir, (50, 50))
+hiir.set_colorkey((0, 0, 0))
+running = True
+hetke_x = 10 
+hetke_y = 10
+kell = pygame.time.Clock()
+delta = 0.1
+font = pygame.font.Font(None, size=30)
+moving_right = moving_left = moving_up = moving_down = False
+vaade_horis = vaade_vert = False
+elud = 3
+mängu_faas = "algus"
+
+LEVELID = {
+    "LEVEL_1" : level_1,
+    "LEVEL_2" : level_2,
+    "LEVEL_3" : level_3,
+    "LEVEL_4" : level_4,
+    "LEVEL_5" : level_5
+}
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #kui ekraan pannakse ristist kinni, siis mäng lõpetab töö
+            running = False
+    if mängu_faas == "QUIT":
+        running = False
+    elif mängu_faas == "algus":
+        print("Tere!")
+        mängu_faas = "LEVEL_1"
+    elif mängu_faas in LEVELID:
+        print(mängu_faas)
+        praeguselevelifunktsioon = LEVELID[mängu_faas]
+        järgmine_state, hetke_x, hetke_y, elud = praeguselevelifunktsioon(screen, hetke_x, hetke_y, elud)
+        mängu_faas = järgmine_state
+    
+    else:
+        print(mängu_faas)
+        print("Viga!!!")
+        running = False
+
+pygame.quit()
+sys.exit()
---
+++ Projekt_level4.py
@@ -0,0 +1,126 @@
+import pygame
+pygame.init()
+import time
+
+screen=pygame.display.set_mode((640, 640)) #mängu ekraani suurus pikslites
+
+hiir = pygame.image.load('hiir.png').convert() #hiire pilt
+hiir = pygame.transform.scale(hiir, (50, 50)) #kuna hiire pilt on suur, teisendame väiksemaks
+hiir.set_colorkey((0, 0, 0)) #hiire taust valge
+kell = pygame.time.Clock()
+delta = 0.1
+font = pygame.font.Font(None, size=30) #font, millega kirjad on mänguekraanil
+
+takistused = [
+        pygame.Rect(200, 100, 100, 100),
+        pygame.Rect(75, 150, 30, 70),
+        pygame.Rect(300, 400, 30, 70),
+        pygame.Rect(250, 100, 100, 100),
+        pygame.Rect(400, 150, 150, 70),
+        pygame.Rect(450, 400, 100, 200),
+        pygame.Rect(100, 300, 200, 50)
+    ]
+def level_4(screen, x_alg, y_alg, elud_alg):
+    x = 0
+    y = 0
+    elud = elud_alg
+    level_running = True
+    moving_right = moving_left = moving_up = moving_down = False
+    
+    while level_running:
+        screen.fill((255, 255, 255)) #mängutaust valgeks
+        screen.blit(hiir, (x, y)) #hiir mänguekraanile
+        #alguse ja lõpu ristkülikud(asukoht, suurus), lisatud ekraanile
+        algus = pygame.Rect(0, 0, 75, 75) 
+        lõpp = pygame.Rect(570, 570, 100, 100)
+        pygame.draw.rect(screen, (255, 0, 0), algus)
+        pygame.draw.rect(screen, (255, 0, 0), lõpp)
+    
+        hiir_rect = pygame.Rect(x, y, 50, 50)
+
+        #takistused ekraanile joonistatud
+        for t in takistused:
+            pygame.draw.rect(screen, (0, 0, 0), t)
+
+        #tekstid ja kirjutame ekraanile
+        tekst = font.render('Algus', True, (0, 0, 0))
+        tekst1 = font.render('Lõpp', True, (0, 0, 0))
+        tekst2 = font.render('Tubli! Said hakkama!', True, (0, 0, 0))
+        tekst3 = font.render('Jõua 4. leveli lõppu! Edu!', True, (0, 0, 0))
+        screen.blit(tekst, (0, 0))
+        screen.blit(tekst1, (570, 570))
+        
+        # elude ekraanile kuvamine 
+        elud_tekst = font.render(f'Elusid: {elud}', True, (0, 0, 255))
+        screen.blit(elud_tekst, (540, 10)) # Asukoht üleval paremal
+        #olukorrakontroll
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: #kui ekraan pannakse ristist kinni, siis mäng lõpetab töö
+                level_running = False
+                return "QUIT", x, y, elud
+            #kui mõni nooleklahv on all vajutatud, siis sõltuvalt kindlast nupust hakkab hiir liikuma
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    moving_right = True
+                if event.key == pygame.K_LEFT:
+                    moving_left = True
+                if event.key == pygame.K_UP:
+                    moving_up = True
+                if event.key == pygame.K_DOWN:
+                    moving_down = True  
+            #kui nupp lahti lastud, siis liikumine on False ja liikumine lõpeb
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    moving_right = False
+                if event.key == pygame.K_LEFT:
+                    moving_left = False
+                if event.key == pygame.K_UP:
+                    moving_up = False
+                if event.key == pygame.K_DOWN:
+                    moving_down = False
+        #liikumise loogika
+        if moving_right:
+            x += 50 * delta
+        if moving_left:
+            x -= 50 * delta
+        if moving_down:
+            y += 50 * delta
+        if moving_up:
+            y -= 50 * delta
+        
+        # Kokkupõrke kontroll ja elu vähendamine
+        collision_detected = False
+        for t in takistused:
+            if hiir_rect.colliderect(t):
+                collision_detected = True
+                break
+
+        if collision_detected:
+            elud -= 1       
+            x = 0
+            y = 0
+            pygame.time.wait(200)
+
+            if elud <= 0:
+                level_running = False # Kui elud otsas, paneb mängu kinni
+                return "QUIT", x, y, elud
+        #alguse ja lõpu tekstide kuvamine
+        if x > 570 and y > 570:
+            screen.blit(tekst2, (320, 320))
+            return "LEVEL_5",  x, y, elud
+        if x < 75 and y < 75:
+            screen.blit(tekst3, (90, 40))
+        #loogika, et hiir ekraanilt välja ei läheks
+        if x < 2:
+            x += 20
+        elif x > 610:
+            x -= 20
+        
+        if y < 2:
+            y += 20
+        elif y > 610:
+            y -= 20
+
+        pygame.display.flip() #värskendab ala, mis on muutunud, mitte tervet ekraani.
+        delta_aeg = kell.tick(60) / 1000 #kahekaadri vaheline aeg millisekundites
+        delta_aeg = max(0.001, min(0.1, delta_aeg))
